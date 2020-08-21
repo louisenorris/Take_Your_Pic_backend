fd238e3c81ae4a56bd090854ae083f29b78955e1
Louise Norris <louisenorris73@yahoo.co.uk>
2020-08-21 14:01:20 +0100

    added date format to en.yml and created_at to polaroid serializer

--- app/serializers/polaroid_serializer.rb
+++ app/serializers/polaroid_serializer.rb
@@ -1,6 +1,6 @@
 class PolaroidSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :user_id, :updated_at, :photo
+  attributes :id, :user_id, :updated_at, :created_at, :photo
 
   def photo
     if object.photo.attached?
